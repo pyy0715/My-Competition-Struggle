7dc3d943b6d98bec0b06f53c883a88e3dca5821b
pyy0715 <zxc077@naver.com>
2021-04-10 00:48:49 +0900

    add submodule

--- Kakao_Arena/categories-prediction/src/train.bat
+++
@@ -1,5 +0,0 @@
-python train.py --stratified --fold 0
-python train.py --stratified --fold 1
-python train.py --stratified --fold 2
-python train.py --stratified --fold 3
-python train.py --stratified --fold 4
